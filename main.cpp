616c694084374c6cd4896ba88f22610fbad1dc4b
Andrey Derevyanko <derandr@gmail.com>
2017-10-15 20:38:27 +0200

    Vertex indecies introduced

--- main.cpp
+++ main.cpp
@@ -10,16 +10,21 @@ int main(int /*argc*/, char ** /*argv*/)
     egl_handler.init();
     egl_handler.egl_from_dispmanx();
 
-    GLfloat vVertices[] = {
-        0.0f, 0.5f, 0.0f,
+    GLfloat vertices[] = {
+        -0.5f, -0.5f, 0.0f,
         1.0f, 0.0f, 0.0f,
 
-        -0.5f, -0.5f, 0.0f,
+        -0.5f, 0.5f, 0.0f,
         0.0f, 1.0f, 0.0f,
 
+        0.5f, 0.5f, 0.0f,
+        0.0f, 0.0f, 1.0f,
+
         0.5f, -0.5f, 0.0f,
-        0.0f, 0.0f, 1.0f
+        1.0f, 1.0f, 0.0f
      };
+
+     GLubyte indecies[] = {0, 1, 2, 2, 3, 0};
 
     glViewport(0, 0, egl_handler.screen_width(), egl_handler.screen_height());
 
@@ -30,11 +35,12 @@ int main(int /*argc*/, char ** /*argv*/)
     glClearColor(0.0, 0.2, 0.2, 1.0);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), vVertices);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), vertices);
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), vVertices + 3);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(GLfloat), vertices + 3);
     glEnableVertexAttribArray(1);
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    //glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_BYTE, indecies);
 
     glFlush();
     eglSwapBuffers(egl_handler.state()->display, egl_handler.state()->surface);
